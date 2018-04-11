867aef5c3af4f030a4f747825ebac6c2a710fe0d
666 <@4d.666>
2018-04-11 17:04:28 +0800

    添加登录和home组建并配置路由导航

--- src/components/home/home.vue
+++ src/components/home/home.vue
@@ -1,13 +1,14 @@
 <template>
-  <div id= "app" >
-    <p>app component</p>
-    <router-view></router-view>
+  <div>
+    <p>home component</p>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  data () {
+    return {}
+  }
 }
 </script>
 
